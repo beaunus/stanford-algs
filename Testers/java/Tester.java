d82c2bbe225bee71900ca399892ca92250a517dc
Beau Dobbin <beau@beaunus.com>
2017-04-03 13:00:09 +0900

    improve code comments

--- Testers/java/Tester.java
+++ Testers/java/Tester.java
@@ -17,11 +17,16 @@ public class Tester {
     String className = args[0];
     System.out.println("Your class name => " + className);
 
+    // Iterate over all the filenames in the command-line arguments.
     for (int i = 1; i < args.length; i++) {
       String inputFilename = args[i];
-      String result = callMain(className, inputFilename);
+      // Infer the output filename from the input filename.
       String outputFilename = inputFilename.replaceAll("input", "output");
 
+      // Get the result of calling the solution class's main() method on this file.
+      String result = callMain(className, inputFilename);
+
+      // Display the results of this test case file.
       System.out.println(" inputFilename => " + inputFilename);
       String expectedResult = null;
       expectedResult = new String(Files.readAllBytes(Paths.get(outputFilename)), "UTF-8");
@@ -49,7 +54,6 @@ public class Tester {
       String[] args = {filename};
 
       // Capture and return the standard output of the main function.
-
       // Create a stream to hold the output
       ByteArrayOutputStream baos = new ByteArrayOutputStream();
       PrintStream ps = new PrintStream(baos);
@@ -60,11 +64,11 @@ public class Tester {
 
       simpleSolution.invoke(null, (Object) args);
 
+      // Return System.out to the initial stream.
       System.out.flush();
       System.setOut(old);
 
       return baos.toString();
-      // production code should handle these exceptions more gracefully
     } catch (ClassNotFoundException x) {
       x.printStackTrace();
     } catch (NoSuchMethodException x) {
