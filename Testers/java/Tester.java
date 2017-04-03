0a1dc2e0a784aa9d8f69b1c50d86935939e354b4
Beau Dobbin <beau@beaunus.com>
2017-04-03 15:57:27 +0900

    Remove unnecessary imports

--- Testers/java/Tester.java
+++ Testers/java/Tester.java
@@ -6,7 +6,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Tester {
