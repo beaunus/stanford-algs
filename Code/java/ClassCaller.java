06edb0ef7f74320631efe4574ec8197bb5618f3e
Beau Dobbin <beau@beaunus.com>
2017-04-10 17:25:43 +0900

    isolated ClassCaller from Tester

---
+++ Code/java/ClassCaller.java
@@ -0,0 +1,74 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * A class to encapsulate methods for calling the main function of a class and returning the
+ * System.out output as a String.
+ */
+public class ClassCaller {
+  /**
+   * Return the main Method of the specified class.
+   *
+   * @param className the class whose main Method should be returned.
+   * @return the main Method
+   */
+  public static Method getMainMethod(String className) {
+    try {
+      // Get the Class object for the specified className.
+      Class<?> myClass = Class.forName(className);
+      // Since we want to get the main Method, the argTypes is a String[].
+      Class<?>[] argTypes = new Class[] {String[].class};
+
+      // Get and return the main Method.
+      Method main = myClass.getDeclaredMethod("main", argTypes);
+      return main;
+    } catch (ClassNotFoundException cnfe) {
+      System.err.println("The specified class name cannot be found.");
+      cnfe.printStackTrace();
+    } catch (NoSuchMethodException nsme) {
+      System.err.println("The specified class does not have a main method.");
+      nsme.printStackTrace();
+    }
+    return null;
+  }
+
+  /**
+   * Call the specified method and return the Standard.out output as a String.
+   *
+   * @param method them method to call
+   * @param args the single arguments to be passed to the method
+   * @return the Standard.out output of the method as a String
+   */
+  public static String callMethod(Method method, String[] args) {
+    try {
+
+      // Capture and return the standard output of the specified method.
+      // Create a stream to hold the output
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      PrintStream ps = new PrintStream(baos, true, "UTF-16");
+      // IMPORTANT: Save the old System.out!
+      final PrintStream old = System.out;
+      // Tell Java to use your special stream
+      System.setOut(ps);
+
+      // Invoke the method.
+      method.invoke(null, (Object) args);
+
+      // Return System.out to the initial stream.
+      System.out.flush();
+      System.setOut(old);
+
+      return baos.toString("UTF-16");
+    } catch (IllegalAccessException iae) {
+      iae.printStackTrace();
+    } catch (InvocationTargetException ite) {
+      ite.printStackTrace();
+    } catch (UnsupportedEncodingException uee) {
+      uee.printStackTrace();
+    }
+    return null;
+  }
+}
