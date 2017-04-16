71e4c5b77471abf6ec23033254b7827ddc88a170
Beau Dobbin <beau@beaunus.com>
2017-04-16 09:18:15 +0900

    moved main method to extending class

--- testCases/course3/assignment2Clustering/question1/TestCaseGenerator.java
+++ testCases/course3/assignment2Clustering/question1/TestCaseGenerator.java
@@ -27,6 +27,51 @@ import utility.TestCaseGeneratorSuperclass;
  */
 public class TestCaseGenerator extends TestCaseGeneratorSuperclass {
 
+  /**
+   * Generate the test cases.
+   *
+   * @param args [method to call] [solver class] {[args to method]}
+   */
+  public static void main(String[] args) {
+    // Confirm that the command-line arguments are valid.
+    if (args.length < 2) {
+      System.out.println("usage:");
+      System.out.println(
+          TestCaseGenerator.class.getName()
+              + " [method to call] [solver class] {[args to method]}");
+      // Display all available methods for generating an input file.
+      System.out.println("\nAvailable methods: ");
+      for (Method method : TestCaseGenerator.class.getDeclaredMethods()) {
+        if (!method.getName().equals("main")) {
+          System.out.println("\t" + method.getName());
+        }
+      }
+      System.out.println();
+      System.exit(0);
+    }
+
+    // Retrieve the main method of the solver class;
+    Method solverMainMethod = ClassCaller.getMainMethod(args[1]);
+
+    // Since we want to get the main Method, the argTypes is a String[].
+    Class<?>[] argTypes = new Class[] {Method.class, String[].class};
+    try {
+      Method methodToCall = TestCaseGenerator.class.getDeclaredMethod(args[0], argTypes);
+      methodToCall.invoke(null, solverMainMethod, Arrays.copyOfRange(args, 2, args.length));
+    } catch (NoSuchMethodException e) {
+      System.out.println("The specified [method to call] does not exist.");
+      e.printStackTrace();
+      System.exit(0);
+    } catch (SecurityException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
+  }
 
   /**
    * Generate 4 unique example files for each of the specified number of vertices. The input file
