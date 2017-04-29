356bdfcc3b909f52e42819355a97b2f38a4dad5f
Beau Dobbin <beau@beaunus.com>
2017-04-29 13:10:39 +0900

    added TestCaseGenerator for 3.1.3

---
+++ testCases/course3/assignment1SchedulingAndMST/question3/TestCaseGenerator.java
@@ -0,0 +1,125 @@
+// Be sure to put the appropriate package name
+
+package testCases.course3.assignment1SchedulingAndMST.question3;
+
+import utility.AbstractTestCaseGenerator;
+import utility.ClassCaller;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * An test case generator for course3 assignment1 question3.
+ *
+ * <p>Using this class's main method should be parameter driven. The convention is as follows:
+ *
+ * <p>java testCases.course4.assignment1AllPairsShortestPath [method to call] [solver class] {[args
+ * to method]}
+ *
+ * <p>The [solver class] is used to ensure that test cases with unique solutions are produced.
+ */
+public class TestCaseGenerator extends AbstractTestCaseGenerator {
+  public TestCaseGenerator(String methodToUse, String solverClassName, String[] args) {
+    super(methodToUse, solverClassName, args);
+  }
+
+  /**
+   * Run the TestCaseGenerator.
+   *
+   * @param args [method to call] [solver class] {[args to method]}
+   */
+  public static void main(String[] args) {
+    AbstractTestCaseGenerator.main(TestCaseGenerator.class, args);
+    TestCaseGenerator tcg =
+        new TestCaseGenerator(args[0], args[1], Arrays.copyOfRange(args, 2, args.length));
+    tcg.generateInputFiles();
+  }
+
+  /**
+   * Creates 4 unique test cases for each of the problem sizes.
+   *
+   * <p>The problem size is the number of nodes.
+   *
+   * @param solverMainMethod the main method that gives a solution with the given file
+   * @param args the filenameStartingIndex, followed by an array of numbers of nodes
+   */
+  public void random(Method solverMainMethod, String[] args) {
+    int filenameStartingIndex = Integer.parseInt(args[0]);
+
+    // Iterate through the command line arguments
+    int argPointer = 1;
+    while (argPointer < args.length) {
+
+      System.out.println("Processing problem size => " + args[argPointer]);
+
+      int numNodes = Integer.parseInt(args[argPointer++]);
+
+      // A set of solutions.
+      // Used to ensure 4 unique test case files are generated.
+      HashSet<String> solutions = new HashSet<String>();
+
+      while (solutions.size() < 4) {
+
+        // Initialize the list of file lines
+        ArrayList<String> lines = new ArrayList<String>();
+
+        int numEdges = 0;
+
+        // Add edges to ensure that the graph is connected
+        for (int i = 1; i < numNodes; i++) {
+          int weight = ThreadLocalRandom.current().nextInt(-9999, 10000);
+          lines.add(i + " " + (i + 1) + " " + weight);
+          numEdges++; 
+        }
+
+        // Add random edges (1/3) of the time.
+        for (int i = 1; i < numNodes; i++) {
+          if (ThreadLocalRandom.current().nextInt(3) == 1) {
+            int otherVertex = ThreadLocalRandom.current().nextInt(numNodes) + 1;
+            int weight = ThreadLocalRandom.current().nextInt(-9999, 10000);
+            lines.add(i + " " + otherVertex + " " + weight);
+          }
+          numEdges++; 
+        }
+        
+        // prepend the array of lines with the calculated number of edges
+        lines.add(0, numNodes + " " + numEdges);
+
+        // Create the filename for the test case.
+        String inputFilename = "input_random";
+        inputFilename += "_" + filenameStartingIndex;
+        inputFilename += "_" + numNodes;
+        inputFilename += ".txt";
+
+        // Write the input file.
+        Path file = Paths.get(inputFilename);
+        try {
+          Files.write(file, lines, Charset.forName("UTF-8"));
+        } catch (IOException exception) {
+          exception.printStackTrace();
+        }
+
+        String[] filenames = {inputFilename};
+
+        String solution = ClassCaller.callMethod(solverMainMethod, filenames);
+
+        // create an output file for this solution only if it is a unique solution.
+        if (!solutions.contains(solution)) {
+          // Add this solution to the set of solutions so far.
+          solutions.add(solution);
+          AbstractTestCaseGenerator.generateOutputFile(inputFilename, solution);
+          System.out.println("\t" + solutions.size() + " unique file(s) generated.");
+          filenameStartingIndex++;
+        }
+      }
+    }
+  }
+}
