b7b83f31c7c70f8c7c80012131e83e13d19a158c
Daniel Gonzalez Reina <dangonrei@gmail.com>
2017-04-29 11:17:44 +0200

    running only the test cases in option 'only'. Modified the README to explain how to use it

--- tester/python3/README.md
+++ tester/python3/README.md
@@ -29,7 +29,7 @@ Running the Tester requires only a single command.  It's long, but you'll get
 the hang of it.
 
 ```shell
-python3 tester.py path/to/your/algorithm.py path/to/testcases options
+python3 tester.py path/to/your/algorithm.py path/to/testcases [options]
 ```
 
 We'll brake down the command:
@@ -38,14 +38,15 @@ We'll brake down the command:
 * `tester.py`: execute the tester
 * `path/to/your/algorithm.py`: this is the path to the file where you have your solution code. By default the tester will try to import the method named `alg` from that file. You can specify a custom name with the options.
 * `path/to/testcases`: this is the path to the folder where you have the test cases
-* `options`: so far there are two possible options (feel free to contribute!)
+* `options`: so far there are three possible options (feel free to contribute!)
 	* `name=METHOD_NAME`: use this option if the method is named different than `alg`
 	* `max_size=SIZE`: use this option if you don't want to run files with input bigger than `SIZE`. This is useful if you're looking for a bug and the algorithm takes too long for large inputs.
+	* `only=FILES`: use this option to run only specific tests. The value to send is a comma-separated-value list of the _references_ of the files you want to test. The reference of a file is the name of the file, without 'input_' and '.txt'. When there are failed tests, you'll get the comma-separated-value list of the references that failed, so it will be easy to re-run only those failed tests. See the tester output if you're not sure how to send the comma-separated-value list of references.
 
-Here's an example using all the options. Imagine you have the file 'Dijkstra.py' in the folder './course/assignment/', there you have the method `shortest_paths` that runs Dijksra's algorithm and returns the solution you are looking for. You have all your tests cases for that algorithm in the folder './course/tests/dijkstra', but you don't want to run the tests with inputs larger than 100000. In that case, you would write
+Here's an example using all the options. Imagine you have the file 'Dijkstra.py' in the folder './course/assignment/', there you have the method `shortest_paths` that runs Dijksra's algorithm and returns the solution you are looking for. You have all your tests cases for that algorithm in the folder './course/tests/dijkstra', but you don't want to run the tests with inputs larger than 100000. Additionally, you'll only want to run the files with references `random_22_100` and `random_23_100`. In that case, you would write
 
 ```shell
-python3 tester.py ./course/assignment/Dijkstra.py ./course/tests/dijkstra name=shortest_paths max_size=100000
+python3 tester.py ./course/assignment/Dijkstra.py ./course/tests/dijkstra name=shortest_paths max_size=100000 only=random_22_100,random_23_100
 ```
 
 ### Tester output
@@ -55,13 +56,20 @@ The tester will give you some feedback before running the tests, to let you know
 ```shell
 Testing algorithm at "PATH/TO/ALGORITHM"
 Options:
-	name=NAME
-	max_size=SIZE
+	name = NAME
+	max_size = SIZE
+	only =
+		fileref_1
+		fileref_2
+		...
 ```
 
-Then, it will print a line for each skipped test, so you can check that the skipping went as expected.
+Then, it will print a line for each skipped test with the reason of skipping, so you can check that the skipping went as expected.
 
-`skipped test case dgrcode_03_1000`
+```shell
+skipped fileref_i 	-> exceeded max_size
+skipped fileref_j 	-> not in only
+```
 
 After that, it will print a quick summary of the test as they are run. The summary has the reference to the file, and shows the expected and the computed results in case they don't match. You'll see "✔" in case a submission was correct or "✘" otherwise. For example, you'll see something like this:
 
@@ -84,4 +92,5 @@ And in case something failed, you'll see:
 ✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘
 Some tests didn't pass
 ✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘
+Failed tests: random_22_100,random_23_100
 ```
--- tester/python3/tester.py
+++ tester/python3/tester.py
@@ -2,7 +2,7 @@ import sys
 import os
 import importlib
 
-def test(student_algorithm, test_cases_folder, name='alg', max_size=-1):
+def test(student_algorithm, test_cases_folder, name='alg', max_size=-1, only=[]):
 	# import the algorithm
 	*alg_path, alg_file = student_algorithm.split('/')
 	sys.path.append('/'.join(alg_path))
@@ -11,10 +11,14 @@ def test(student_algorithm, test_cases_folder, name='alg', max_size=-1):
 
 	# print the options to the user
 	print('Testing algorithm at "'+ student_algorithm + '".')
-	if name != 'alg' or max_size != -1:
+	if name != 'alg' or max_size != -1 or only != []:
 		print('Options: ')
 		if name != 'alg': print('\tname =', name)
 		if max_size != -1: print('\tmax_size =', max_size)
+		if len(only) > 0:
+			print('\tonly =')
+			for only_file in only:
+				print('\t\t' + only_file)
 
 	# read the test cases titles
 	test_cases = []
@@ -25,15 +29,20 @@ def test(student_algorithm, test_cases_folder, name='alg', max_size=-1):
 		file_type, file_user, file_num, file_size = filename[:-4].split('_')
 		file_size = int(file_size)
 		fileref = [file_user, file_num, file_size]
+		fileref_str = '_'.join([str(x) for x in fileref])
+
+		# filter 'only' files
+		if len(only)>0 and fileref_str not in only:
+			print('skipped', fileref_str, '\t-> not in only')
+			continue
 
 		# if the size is bigger than the max_size option, don't run that test
 		if max_size != -1 and file_size > max_size:
-			print('skipped test case ' + '_'.join([str(x) for x in fileref]))
+			print('skipped', fileref_str, '\t-> exceeded max_size')
 			continue
 		
 		# construct the output
-		output_file = (test_cases_folder +
-				'/output_' + '_'.join([str(x) for x in fileref]) + '.txt')
+		output_file = (test_cases_folder + '/output_' + fileref_str + '.txt')
 		# read the expected output
 		with open(output_file, 'r') as ofile:
 			output = []
@@ -50,8 +59,10 @@ def test(student_algorithm, test_cases_folder, name='alg', max_size=-1):
 	print()
 	tests_passed = 0
 	tests_total = 0
+	failed_tests = []
 	for fileref, expected_output in test_cases:
-		filename = test_cases_folder + '/input_' + '_'.join(str(x) for x in fileref) + '.txt'
+		fileref_str = '_'.join(str(x) for x in fileref)
+		filename = test_cases_folder + '/input_' + fileref_str + '.txt'
 
 		alg_output_unprocessed = alg(filename)
 		if not isinstance(alg_output_unprocessed, list):
@@ -66,18 +77,18 @@ def test(student_algorithm, test_cases_folder, name='alg', max_size=-1):
 			return
 
 		answers_ok = []
-		print('_'.join([str(x) for x in fileref]), end='\t')
+		print(fileref_str, end='\t')
 		errors_buffer = ''
 		for i in range(len(alg_output)):
 			if expected_output[i] == alg_output[i]:
 				answers_ok.append(True)
-				#print('\tExpected: ' + expected_output[i] + '\n\tResult: ' + alg_output[i], end='\t')
 				print(u'\N{heavy check mark}', end='')
 			else:
 				answers_ok.append(False)
 				print(u'\N{heavy ballot x}', end='')
 				errors_buffer += ('\n    Answer ' + str(i+1) + ':\n\tExpected: ' + expected_output[i] +
 					'\n\tResult: ' + alg_output[i])
+				failed_tests.append(fileref_str)
 
 		if all(answers_ok):
 			tests_passed += 1
@@ -99,14 +110,16 @@ def test(student_algorithm, test_cases_folder, name='alg', max_size=-1):
 		print(u'\N{heavy ballot x}'*len(message))
 		print(message)
 		print(u'\N{heavy ballot x}'*len(message))
+		print('Failed tests:', ','.join(failed_tests))
 
 
 if __name__ == '__main__':
 	tester_name, student_algorithm, test_cases_folder, *options = sys.argv
 	
-	valid_options = ['max_size', 'name']
-	max_size = -1
+	valid_options = ['max_size', 'name', 'only']
 	name = 'alg'
+	max_size = -1
+	only = []
 	for opt in options:
 		opt_name, opt_value = opt.split('=')
 		if opt_name == 'max_size':
@@ -115,9 +128,12 @@ if __name__ == '__main__':
 		if opt_name == 'name':
 			name = opt_value
 
+		if opt_name == 'only':
+			only = opt_value.split(',')
+
 		if opt_name not in valid_options:
 			print('Unkown option "' + opt_name + '". Valid options are\n\t"name"\n\t"max_size"')
 			print('Option "'+ opt_name +'" not set')
 
-	option_values = [name, max_size]
+	option_values = [name, max_size, only]
 	test(student_algorithm, test_cases_folder,  *option_values)
