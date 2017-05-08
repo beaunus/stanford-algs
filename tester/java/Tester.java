2237d0418b7e94d6c311a5264e9aa3332262e448
Beau Dobbin <beau@beaunus.com>
2017-05-08 14:30:55 +0900

    added colors in console output

--- tester/java/Tester.java
+++ tester/java/Tester.java
@@ -34,8 +34,9 @@ public class Tester {
     if (args.length < 2) {
       System.out.println(
           "Usage:"
+              + "java -cp <path to tester class>:<path to solution class> "
               + Tester.class.getName()
-              + " [solution class name] {[list of input files]} [-options]");
+              + "  [-options] <solution class name> <list of input files>");
       System.out.println("where options include:");
       System.out.println("\t-maxsize        maximum input size");
       System.out.println("\t-name           method that returns solution (default: main)");
@@ -44,44 +45,59 @@ public class Tester {
     }
 
     // Parse the arguments.
-    HashMap<String, String> arguments = new HashMap<String, String>();
-    ArrayList<String> filenames = new ArrayList<String>();
-    arguments.put("solutionClassName", args[0]);
+    int argPointer = 0;
 
-    for (int i = 1; i < args.length; i++) {
-      if (args[i].startsWith("-")) {
-        int indexOfColon = args[i].indexOf(":");
-        String parameterName = args[i].substring(1, indexOfColon);
-        String argument = args[i].substring(indexOfColon + 1, args[i].length());
-        arguments.put(parameterName, argument);
-      } else {
-        filenames.add(args[i]);
-      }
+    // Parse the options.
+    HashMap<String, String> options = new HashMap<String, String>();
+    while (args[argPointer].startsWith("-")) {
+      int indexOfColon = args[argPointer].indexOf(":");
+      String parameterName = args[argPointer].substring(1, indexOfColon);
+      String argument = args[argPointer].substring(indexOfColon + 1, args[argPointer].length());
+      options.put(parameterName, argument);
+      argPointer++;
     }
 
-    String className = arguments.get("solutionClassName");
+    // Parse the solution class name.
+    String solutionClassName = args[argPointer++];
 
-    System.out.println("Testing your code's output with the expected output.");
-    System.out.println();
-    for (Entry<String, String> entry : arguments.entrySet()) {
+    // Parse the file names.
+    ArrayList<String> filenames = new ArrayList<String>();
+    while (argPointer < args.length) {
+      if (validateFilename(args[argPointer])) {
+        filenames.add(args[argPointer]);
+      }
+      argPointer++;
+    }
+
+    // Display feedback to user.
+    System.out.println("\nTesting your code's output with the expected output.\n");
+    // Display options.
+    for (Entry<String, String> entry : options.entrySet()) {
       System.out.println(entry.getKey() + " => " + entry.getValue());
     }
+    System.out.println();
 
     // Retrieve the main method of the solution class.
-    String methodName = arguments.get("name") != null ? arguments.get("name") : "main";
-    Method method = ClassCaller.getMethod(className, methodName);
+    String methodName = options.get("name") != null ? options.get("name") : "main";
+    Method method = ClassCaller.getMethod(solutionClassName, methodName);
 
+    // Sort the filenames, so simpler problems are processed first.
     filenames.sort(new InputSizeOrder());
+
+    // Initialize an array of failed tests, to be displayed later.
+    ArrayList<String> failedTests = new ArrayList<String>();
 
     // Iterate over all the filenames in the command-line arguments.
     for (String inputFilename : filenames) {
       String[] filenameStringArray =
           inputFilename.substring(0, inputFilename.indexOf(".")).split("_");
 
-      // Determine if this input file exceeds the maxsize
-      if (arguments.get("maxsize") != null) {
-        if (Integer.parseInt(arguments.get("maxsize")) < Integer.parseInt(filenameStringArray[3])) {
+      // Determine if this input file exceeds the maxsize.
+      if (options.get("maxsize") != null) {
+        if (Integer.parseInt(options.get("maxsize")) < Integer.parseInt(filenameStringArray[3])) {
+          System.out.print("\033[36m");
           System.out.println("skipped " + inputFilename + "\t-> exceeded maxsize");
+          System.out.print("\033[0m");
           continue;
         }
       }
@@ -89,6 +105,7 @@ public class Tester {
       // Infer the output filename from the input filename.
       String outputFilename = inputFilename.replaceAll("input", "output");
 
+      // Retrieve the expected result.
       String expectedResult = null;
       try {
         expectedResult = new String(Files.readAllBytes(Paths.get(outputFilename)), "UTF-8");
@@ -99,14 +116,53 @@ public class Tester {
       // Get the result of calling the solution class's main() method on this file.
       String[] thisFilenameAsArray = {inputFilename};
       final String result = ClassCaller.callMethod(method, thisFilenameAsArray);
+
+      // Display whether or not this test matches the expected results.
       if (expectedResult != null && result.trim().equals(expectedResult.trim())) {
+        System.out.print("\033[32m");
         System.out.println(inputFilename + "\t✔");
+        System.out.print("\033[0m");
       } else {
+        System.out.print("\033[31m");
         System.out.println(inputFilename + "\t✘");
         System.out.println("\tExpected: " + expectedResult.trim());
         System.out.println("\t  Result: " + result.trim());
+        System.out.print("\033[0m");
+        failedTests.add(inputFilename);
       }
     }
+
+    // Display summary of test results.
+    System.out.println();
+    if (failedTests.isEmpty()) {
+      System.out.print("\033[32m");
+      System.out.println("✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔");
+      System.out.println("All tests passed");
+      System.out.println("✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔");
+      System.out.print("\033[0m");
+    } else {
+      System.out.print("\033[31m");
+      System.out.println("✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘");
+      System.out.println("Some tests didn't pass");
+      System.out.println("✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘✘");
+      System.out.println("\nFailed tests:");
+      for (String failedTest : failedTests) {
+        System.out.println("\t" + failedTest);
+      }
+      System.out.print("\033[0m");
+    }
+  }
+
+  private static boolean validateFilename(String string) {
+    if (string.split("_").length == 4) {
+      return true;
+    }
+    System.out.println(
+        "'"
+            + string
+            + "'"
+            + " must match the pattern '\033[1minput_description_index_problemSize.txt\033[0m'");
+    return false;
   }
 
   private static class InputSizeOrder implements Comparator<String> {
@@ -115,6 +171,7 @@ public class Tester {
     public int compare(String o1, String o2) {
       String[] o1AsArray = o1.split("_");
       String[] o2AsArray = o2.split("_");
+
       int o1Size = Integer.parseInt(o1AsArray[o1AsArray.length - 2].split("\\.")[0]);
       int o2Size = Integer.parseInt(o2AsArray[o2AsArray.length - 2].split("\\.")[0]);
 
