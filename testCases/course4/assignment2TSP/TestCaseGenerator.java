88d91b1ec0be2e83de8a6ec53ccaf2e476d3527c
Beau Dobbin <beau@beaunus.com>
2017-05-10 10:57:01 +0900

    renames 4.2 files to allow floats and ints

--- testCases/course4/assignment2TSP/TestCaseGenerator.java
+++ testCases/course4/assignment2TSP/TestCaseGenerator.java
@@ -46,12 +46,83 @@ public class TestCaseGenerator extends AbstractTestCaseGenerator {
    *
    * <p>The problem size defines the number of "cities" in the tour.
    *
-   * <p>The "cities'" coordinates range from [0, numberOfCites].
+   * <p>The "cities'" coordinates range from [0, numberOfCites^2]. The "cities'" coordinates are
+   * floating point numbers.
    *
    * @param solverMainMethod the main method that gives a solution with the given file
    * @param args the filenameStartingIndex, followed by an array of numbers of cities.
    */
-  public void simple(Method solverMainMethod, String[] args) {
+  public void intCoordinates(Method solverMainMethod, String[] args) {
+    int filenameStartingIndex = Integer.parseInt(args[0]);
+
+    // Iterate through the command line arguments
+    int argPointer = 1;
+    while (argPointer < args.length) {
+
+      System.out.println("Processing problem size => " + args[argPointer]);
+
+      int numCities = Integer.parseInt(args[argPointer++]);
+
+      // A set of solutions.
+      // Used to ensure 4 unique test case files are generated.
+      HashSet<String> solutions = new HashSet<String>();
+
+      while (solutions.size() < 4) {
+
+        // Initialize the list of file lines
+        ArrayList<String> lines = new ArrayList<String>();
+
+        int maxCoordinate = (int) Math.pow(numCities,2);
+
+        lines.add("" + numCities);
+        for (int i = 0; i < numCities; i++) {
+          int xCoord = ThreadLocalRandom.current().nextInt(maxCoordinate);
+          int yCoord = ThreadLocalRandom.current().nextInt(maxCoordinate);
+          lines.add(xCoord + " " + yCoord);
+        }
+
+        // Create the filename for the test case.
+        String inputFilename = "input_int2";
+        inputFilename += "_" + filenameStartingIndex;
+        inputFilename += "_" + numCities;
+        inputFilename += ".txt";
+
+        // Write the input file.
+        Path file = Paths.get(inputFilename);
+        try {
+          Files.write(file, lines, Charset.forName("UTF-8"));
+        } catch (IOException exception) {
+          exception.printStackTrace();
+        }
+
+        String[] filenames = {inputFilename};
+
+        String solution = ClassCaller.callMethod(solverMainMethod, filenames);
+
+        // create an output file for this solution only if it is a unique solution.
+        if (!solutions.contains(solution)) {
+          // Add this solution to the set of solutions so far.
+          solutions.add(solution);
+          AbstractTestCaseGenerator.generateOutputFile(inputFilename, solution);
+          System.out.println("\t" + solutions.size() + " unique file(s) generated.");
+          filenameStartingIndex++;
+        }
+      }
+    }
+  }
+
+  /**
+   * Creates 4 unique test cases for each of the problem sizes.
+   *
+   * <p>The problem size defines the number of "cities" in the tour.
+   *
+   * <p>The "cities'" coordinates range from [0, numberOfCites]. The "cities'" coordinates are
+   * floating point numbers.
+   *
+   * @param solverMainMethod the main method that gives a solution with the given file
+   * @param args the filenameStartingIndex, followed by an array of numbers of cities.
+   */
+  public void floatCoordinates(Method solverMainMethod, String[] args) {
     int filenameStartingIndex = Integer.parseInt(args[0]);
 
     // Iterate through the command line arguments
@@ -81,7 +152,7 @@ public class TestCaseGenerator extends AbstractTestCaseGenerator {
         }
 
         // Create the filename for the test case.
-        String inputFilename = "input_simple";
+        String inputFilename = "input_float";
         inputFilename += "_" + filenameStartingIndex;
         inputFilename += "_" + numCities;
         inputFilename += ".txt";
